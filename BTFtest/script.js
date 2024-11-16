74c425482d3ba8f5ca6f63b742c8fcb949544a70
WPeytz <98979428+WPeytz@users.noreply.github.com>
2024-11-16 20:37:42 +0100

    saturday 2030 commit

--- BTFtest/script.js
+++
@@ -1,7 +0,0 @@
-// Get the button element
-const button = document.getElementById("pressMeButton");
-
-// Add an event listener to change the text when the button is clicked
-button.addEventListener("click", function () {
-    button.innerText = "OK";
-});
